1d9a9ff2a57701f8597978b8081758fb194a0027
fbatera1998 <83172085+fbatera1998@users.noreply.github.com>
2021-05-18 09:44:03 -0300

    Add files via upload

---
+++ Java script/script.js
@@ -0,0 +1,9 @@
+
+function Soma() {
+  var a = document.getElementsById("var").innerHTML;
+  return a++;
+}
+function Subtrai(){
+  var a = document.getElementsById("var").innerHTML;
+  return a--;
+}
